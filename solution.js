f732dab6a945e9ee92391240dc1ebb2c533411ee
ScottDenton <scottyd_14@hotmail.com>
2019-03-15 16:04:29 -0700

    done

--- solution.js
+++ solution.js
@@ -1,1 +1,19 @@
+// function checkSum(data){
+//   return data.map(array => {
+//     return(Math.max(array) - Math.min(array))
+//   })
+// .reduce((total, int) => int + total)
+//   }
 
+
+checkSum = (d) => d.map(array =>(Math.max(...array) - Math.min(...array))).reduce((total, int) => int + total)
+var testData = [
+  
+  [409, 194, 207, 470, 178],
+  [454, 235, 333, 511, 103],
+  [474, 293, 525, 372, 408],
+  [428, 4321, 2786, 6683, 3921],
+  [265, 262, 6206, 2207, 5712]
+]
+
+console.log(checkSum(testData))
